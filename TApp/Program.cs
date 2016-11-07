e1e5b8c0590455c318d876b36d95121a5cad5792
qweasdd <xavinab@mail.ru>
2016-11-07 23:20:53 +0300

    qwe

--- TApp/Program.cs
+++ TApp/Program.cs
@@ -13,32 +13,16 @@ namespace TApp
     {
         static void Main(string[] args)
         {
-            //Q();
+            
             Work();
             Console.ReadLine();
         }
 
-        private  static void Work()
+        private static void Work()
         {
             var context = new DatabaseEntities();
             GHDownloader ghd = new GHDownloader(context);
             ghd.Download();
-         }
-
-        private static void Q()
-        {
-            var context = new DatabaseEntities();
-            List<string> list = new List<string>();
-            foreach (var item in context.Sourses.First().Languages)
-            {
-                var temp = item.Extentions.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-                list.AddRange(temp);
-            }
-            foreach (var item in list)
-            {
-                Console.WriteLine(item);
-            }
         }
-        
     }
 }
